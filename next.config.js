856c81f8ec0da828093754826c60438436cd39c4
Jon Burbridge <jon.burbridge@playsportsnetwork.com>
2024-01-31 09:44:35 +0000

    minimal reproduction

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const webpack = require("webpack");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^next\/(navigation|headers|compat\/router)$/,
+      }),
+    );
+    return config;
+  },
 }
 
 module.exports = nextConfig
